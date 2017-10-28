d4c625c39dedf2ce9fcdf009a7a41317cbd4590d
Adam Heinrich <adam@adamh.cz>
2017-10-28 22:54:27 +0200

    stm32-cube: Add a header file to satisty the "Missing prototype" warning

--- src/stm32-cube/main.c
+++ src/stm32-cube/main.c
@@ -1,4 +1,5 @@
 #include <stm32f0xx.h>
+#include "stm32f0xx_it.h"
 
 #define LED_PIN 5
 
---
+++ src/stm32-cube/stm32f0xx_it.h
@@ -0,0 +1,6 @@
+#ifndef __STM32F0XX_IT_H__
+#define __STM32F0XX_IT_H__
+
+void SysTick_Handler(void);
+
+#endif
