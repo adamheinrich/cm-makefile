451a9ef835e181058afe214b812d2e049514ea07
Adam Heinrich <adam@adamh.cz>
2017-12-26 22:28:39 +0100

    blackmagic: Add support for the Black Magic Probe

---
+++ blackmagic.mk
@@ -0,0 +1,92 @@
+##
+##  This file is part of CM-Makefile.
+##
+##  Copyright (C) 2015 Adam Heinrich <adam@adamh.cz>
+##
+##  CM-Makefile is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Lesser General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  CM-Makefile is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Lesser General Public License for more details.
+##
+##  You should have received a copy of the GNU Lesser General Public License
+##  along with CM-Makefile.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+# Black Magic Probe target configuration
+BLACKMAGIC_PORT ?= /dev/ttyACM0
+BLACKMAGIC_TPWR ?= 0
+
+BLACKMAGIC_GDBINIT ?= \
+	set mi-async on\n\
+	target extended-remote $(BLACKMAGIC_PORT)\n\
+	set confirm off\n\
+	monitor version\n\
+	$(if $(filter 1,$(BLACKMAGIC_TPWR)),,\#)monitor tpwr enable\n\
+	monitor swdp_scan\n\
+	attach 1\n\
+	file $(realpath $(BUILD_DIR)/$(BIN).elf)\n\
+	load
+
+HELP_TEXT += \n\
+  flash - Flash using Black Magic Probe\n\
+  reset - Reset the target MCU using Black Magic Probe\n\
+  erase - Erase flash (STM32 only) using Black Magic Probe\n\
+  debug - Start debugger and connect to the GDB server
+
+OUTPUTS += $(BUILD_DIR)/$(BIN).gdb $(BUILD_DIR)/$(BIN).cdt
+
+$(BUILD_DIR)/$(BIN).gdb:
+	@echo "  ECHO    $(notdir $@)"
+	$(CMD_ECHO) echo "$(BLACKMAGIC_GDBINIT)" > $@
+
+$(BUILD_DIR)/$(BIN).cdt: $(BUILD_DIR)/$(BIN).gdb
+	@echo "  ECHO    $(notdir $@)"
+	$(CMD_ECHO) echo "org.eclipse.cdt.dsf.gdb/defaultGdbCommand=$(GDB)\n"\
+	"org.eclipse.cdt.dsf.gdb/defaultGdbInit=$(realpath $^)\n" > $@
+
+.PHONY: flash
+flash: $(BUILD_DIR)/$(BIN).hex
+	$(CMD_ECHO) $(GDB) -nx --batch \
+	-ex 'target extended-remote $(BLACKMAGIC_PORT)' \
+	-ex 'set confirm off' \
+	-ex 'monitor version' \
+	$(if $(filter 1,$(BLACKMAGIC_TPWR)),-ex 'monitor tpwr enable') \
+	-ex 'monitor swdp_scan' \
+	-ex 'attach 1' \
+	-ex 'load' \
+	-ex 'compare-sections' \
+	-ex 'kill' \
+	$^
+
+.PHONY: reset
+reset:
+	$(CMD_ECHO) $(GDB) -nx --batch \
+	-ex 'target extended-remote $(BLACKMAGIC_PORT)' \
+	-ex 'set confirm off' \
+	-ex 'monitor version' \
+	$(if $(filter 1,$(BLACKMAGIC_TPWR)),-ex 'monitor tpwr enable') \
+	-ex 'monitor swdp_scan' \
+	-ex 'attach 1' \
+	-ex 'kill'
+
+.PHONY: erase
+erase:
+	$(CMD_ECHO) $(GDB) -nx --batch \
+	-ex 'target extended-remote $(BLACKMAGIC_PORT)' \
+	-ex 'set confirm off' \
+	-ex 'monitor version' \
+	$(if $(filter 1,$(BLACKMAGIC_TPWR)),-ex 'monitor tpwr enable') \
+	-ex 'monitor swdp_scan' \
+	-ex 'attach 1' \
+	-ex 'monitor erase' \
+	-ex 'kill'
+
+.PHONY: debug
+debug: $(BUILD_DIR)/$(BIN).elf | $(BUILD_DIR)/$(BIN).cdt
+	@echo "Starting cdtdebug with $(GDB)..."
+	$(CMD_ECHO) $(CDTDEBUG) -pluginCustomization $| -e $(realpath $^) &
